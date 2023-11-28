1958cb9982e5851684c508d31e34dd3d28573b0c
GAURAV03A <gauravsisodiya999@gmail.com>
2023-11-28 12:56:55 +0530

    vowel

---
+++ vowel.js
@@ -0,0 +1,26 @@
+var char ="q"
+
+if (char=="a" )
+{
+    console.log("vowel");
+}
+else if (char=="e")
+{
+    console.log("vowel");
+}
+else if (char=="i")
+{
+    console.log("vowel");
+}
+else if (char=="o")
+{
+    console.log("vowel");
+}
+else if (char=="u")
+{
+    console.log("vowel");
+}
+else 
+{
+    console.log("not a vowel")
+}
