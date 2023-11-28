1c80b8ae1fc4ed0f1740fcf6954a285bc43c81de
GAURAV03A <gauravsisodiya999@gmail.com>
2023-11-28 12:58:45 +0530

    marryage

---
+++ index1.js
@@ -0,0 +1,25 @@
+var gender = "male" ;
+var age = 17;
+
+if (gender == "male")
+{
+ if ( age>=21 )
+ {
+    console.log ("males:get marry");
+ }
+ else 
+ {
+    console.log("males:can't get marry");
+ }
+}
+
+
+else
+{
+if( age>=18)
+    console.log ("females : get marry");
+else
+{
+    console.log("females : can't get marry");
+}
+}
