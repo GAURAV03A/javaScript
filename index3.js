4b29b044bbfb2351d25a882ffdada5886b5d809c
GAURAV03A <gauravsisodiya999@gmail.com>
2023-11-28 13:05:01 +0530

    conditionStatement

---
+++ index3.js
@@ -0,0 +1,20 @@
+var riceAvailable = false ;
+var wheatAvailable = true;
+var appleAvailable = true;
+
+if(riceAvailable)
+{
+    console.log("buy rice");
+}
+else if(wheatAvailable)
+{
+    console.log("buy wheat");
+}
+else if(appleAvailable)
+{
+    console.log("buy apple");
+}
+else 
+{
+    console.log("nothing is available");
+}
