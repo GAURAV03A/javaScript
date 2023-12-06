2e0d6ca9c49af66af6fb94f5cc5df9d683460e3d
GAURAV03A <gauravsisodiya999@gmail.com>
2023-12-06 18:26:46 +0530

    array

---
+++ array.js
@@ -0,0 +1,16 @@
+var fruits=['mango','apple','kiwi']
+console.log(fruits)
+
+var size=fruits.length
+console.log(size)
+
+fruits.push('orange')
+console.log(fruits)
+
+fruits.pop()
+console.log(fruits)
+
+for(i=0;i<fruits.length;i++)
+{
+    console.log(fruits[i])
+}
