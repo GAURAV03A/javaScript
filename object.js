49f0233e66e7612babfaf05079f7c646186164a6
GAURAV03A <gauravsisodiya999@gmail.com>
2023-12-07 21:18:37 +0530

    object

---
+++ object.js
@@ -0,0 +1,33 @@
+// // var person = {
+// // name : "suraj",
+// // age : 25,
+// // height : "5 ft 12 in",
+// // gender : "male",
+// // hobbies : ["coding", "gyming","eating"]
+// // }
+// // console.log(person)
+
+
+// var person = {
+//     name : "suraj",
+//     age : "20",
+//     address : "MR 10"
+// }
+// console.log(person)
+
+
+// using Array
+
+var person = {
+    name : "suraj",
+    age : "20",
+    address : "MR 10",
+    hobbies : ["coding", "gyming","eating", "traveling"],
+    game : ["cricket","football","chess","kho-kho"]
+}
+console.log(person.game)
+
+var arr = [];
+arr.push(person)
+console.log(arr)
+console.log(person.hobbies[1])
