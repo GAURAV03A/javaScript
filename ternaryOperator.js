9dd2517dc3ea97d5a395ceb80b4ab6dcd0605fee
GAURAV03A <gauravsisodiya999@gmail.com>
2023-12-07 20:37:23 +0530

    ternaryOP

---
+++ ternaryOperator.js
@@ -0,0 +1,17 @@
+// let n = 10;
+// let message;
+
+// // using if-else statement to check if the number is even or odd
+// if(n % 2 ===0)
+// {
+//     message="even";
+// }
+// else{
+//     message="odd";
+// }
+// console.log(message);
+
+let n =10;
+// using the ternary operator to check if the number is even or odd
+let message = n % 2 === 0 ? "even" : "odd"
+console.log(message)
