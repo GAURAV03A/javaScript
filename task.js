10b80001fc45666121e477e3946537bcb3add2a0
GAURAV03A <gauravsisodiya999@gmail.com>
2023-12-27 15:51:15 +0530

    pro

--- task.js
+++ task.js
@@ -1,94 +1,88 @@
 let r= document.querySelector('form');
 let arr=[]
-// console.log(r)
-r.addEventListener('Submit',put);
+// console.log(r)       
+r.addEventListener('submit',put);
 
 function put(event){
     event.preventDefault();
     
-    let name=document.getElementById('x1').value
-    let empID=document.getElementById('x2').value
-    let dept=document.getElementById('Department').value
-    let exp=document.getElementById('x3').value
-    let email=document.getElementById('x4').value
-    let mobile=document.getElementById('x5').value
-    
+    let name=document.getElementById('x1').value;
+    console.log(name)
+    let empID=document.getElementById('x2').value;
+    let dept=document.getElementById('Department').value;
+    let exp=document.getElementById('x3').value;
+    let email=document.getElementById('x4').value;
+    let mobile=document.getElementById('x5').value;
+
     let object={
         Dname:name,
         DempID:empID,
         Ddept:dept,
         Dexp:exp,
         Demail:email,
-        Dmobile:mobile,
+        Dmobile:mobile
     }
     arr.push(object)
-    console.log(object)
+    console.log(arr)
     display(arr)
 }
-// function display(object){
-//     document.querySelector("tbody").innerHTML=""
-//     arr.forEach(function(object)
-//     {
-//         let row=document.createElement("tr")
 
-//         let fname=document.createElement("td")
-//         fname.innerText=object.Dname;
+function display(object){
+    document.querySelector("tbody").innerHTML=""
+    arr.forEach(function(object)
+    {
+        let row=document.createElement("tr")
 
-//         let id=document.createElement("td")
-//         id.innerText=object.DempID
+        let fname=document.createElement("td")
+        fname.innerText=object.Dname;
 
-//         let Depart=document.createElement("td")
-//         Depart.innerText=object.Ddept
+        let id=document.createElement("td")
+        id.innerText=object.DempID
 
-//         let expr=document.createElement("td")
-//         expr.innerText=object.Dexp
+        let Depart=document.createElement("td")
+        Depart.innerText=object.Ddept
 
-//         let Email=document.createElement("td")
-//         Email.innerText=object.Demail
+        let expr=document.createElement("td")
+        expr.innerText=object.Dexp
 
-//         let mob=document.createElement("td")
-//         mob.innerText=object.Dmobile
+        let Email=document.createElement("td")
+        Email.innerText=object.Demail
 
-//         row.append(fname,id,Depart,expr,Email,mob)
-//         document.querySelector("tbody").append(row)
-//     })
-    
-// }
+        let mob=document.createElement("td")
+        mob.innerText=object.Dmobile
 
+        
 
+        let role=document.createElement("td");
+        let roleText=giveRole(object.Dexp);
+        role.innerText=roleText;
 
-
-
-//         let role=document.createElement("td");
-//         let roleText=giveRole(object.Dexp);
-//         role.innerText=roleText;
-
-//         let deleteElement=document.createElement("td");
-//         deleteElement.innerText="delete"
+        let deleteElement=document.createElement("td");
+        deleteElement.innerText="delete"
     
 
-        // deleteElement.addEventListener("click",function(event){
-        //     event.target.parentNode.remove()
-        // })
-        // deleteElement.style.backgroundColor="red"
+        deleteElement.addEventListener("click",function(event){
+            event.target.parentNode.remove()
+        })
+        deleteElement.style.backgroundColor="red"
         
         
-        //     document.querySelector("tbody").append(tr);
-    // });
-// }
+            document.querySelector("tbody").append(row);
 
-// function giveRole(Dexp)
-// {
-//     if(Dexp>5){
-//         return"Senior";
-//     }
-//     else if(Dexp>=2 && Dexp<=5){
-//         return"Junior"
-//     }
-//     else if(Dexp<=1){
-//         return"Fresher"
-//     }
-// }
+            row.append(fname,id,Depart,expr,Email,mob,role,deleteElement)
+            document.querySelector("tbody").append(row)
+    });
+}
 
-
-
+function giveRole(Dexp)
+{
+    if(Dexp>5){
+        return"Senior";
+    }
+    else if(Dexp>=2 && Dexp<=5){
+        return"Junior"
+    }
+    else if(Dexp<=1){
+        return"Fresher"
+    }
+}
